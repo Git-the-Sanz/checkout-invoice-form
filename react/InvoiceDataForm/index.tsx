c1214b541f81bd38be48297192fd43f5cc35edad
María Pizarro Cuevas <maria.pizarro@acct.global>
2021-07-07 17:37:20 +0200

    Ajustes tras revisión con Team

--- react/InvoiceDataForm/index.tsx
+++ react/InvoiceDataForm/index.tsx
@@ -4,11 +4,11 @@ import { Button, Divider, EXPERIMENTAL_Select, Toggle } from 'vtex.styleguide'
 
 const localeOptions = [
   {
-    value: 'it',
+    value: { id: 'it', name: 'it' },
     label: <FormattedMessage id="invoice-data-settings.locale.it" />,
   },
   {
-    value: 'fr',
+    value: { id: 'fr', name: 'fr' },
     label: <FormattedMessage id="invoice-data-settings.locale.fr" />,
   }
 ]
@@ -47,7 +47,8 @@ export default class InvoiceDataForm extends Component {
   constructor(props: any) {
     super(props)
     this.state = {
-      locale: '',
+      locale: [],
+      itSelected: false,
       showInvoiceForm: false,
       invoiceDataMandatory: false,
       showSDIPECSelector: false,
@@ -59,9 +60,16 @@ export default class InvoiceDataForm extends Component {
     }
   }
 
+  checkLanguageSelected = (options: []) => {
+    const optionIT = options.filter((option: any) => option.value.id === 'it')
+
+    this.setState({ itSelected: (optionIT.length > 0) })
+  }
+
   public render() {
     const {
       locale,
+      itSelected,
       showInvoiceForm,
       invoiceDataMandatory,
       showSDIPECSelector,
@@ -94,33 +102,49 @@ export default class InvoiceDataForm extends Component {
             options={localeOptions}
             multi={true}
             onChange={(option: any) => {
-              this.setState({ locale: option.value })
+              this.setState({ locale: option })
+              this.checkLanguageSelected(option)
             }}
-            value={localeOptions.filter((e) => e.value === locale)}
+            defaultValue={localeOptions.filter((e) => e.value === locale)}
           />
         </div>
-        <div className="mv6">
-          <Divider orientation="horizontal" />
-        </div>
-        <div className="mb5">
-          <Toggle
-            label={<FormattedMessage id="invoice-data-settings.sdi-pec" />}
-            checked={showSDIPECSelector}
-            onChange={() => { this.setState({ showSDIPECSelector: !showSDIPECSelector }) }}
-          />
-        </div>
-        <div className="mb3">
-          <EXPERIMENTAL_Select
-            label={<FormattedMessage id="invoice-data-settings.default" />}
-            options={sdiPecOptions}
-            multi={false}
-            onChange={(option: any) => {
-              this.setState({ defaultSDIPEC: option.value })
-            }}
-            value={sdiPecOptions.filter((e) => e.value === defaultSDIPEC)}
-          />
-        </div>
-        <div className="mv6">
+        {itSelected ?
+          <React.Fragment>
+            <div className="mv5">
+              <Divider orientation="horizontal" />
+            </div>
+            <div className="mb4">
+              <div className="t-small c-warning">
+                {<FormattedMessage id="invoice-data-settings.sdi-pec.info-it" />}
+              </div>
+            </div>
+            <div className="mb5">
+              <Toggle
+                label={<FormattedMessage id="invoice-data-settings.sdi-pec" />}
+                checked={showSDIPECSelector}
+                onChange={() => { this.setState({ showSDIPECSelector: !showSDIPECSelector }) }}
+              />
+            </div>
+            <div className="mb3">
+              <EXPERIMENTAL_Select
+                label={<FormattedMessage id="invoice-data-settings.default" />}
+                options={sdiPecOptions}
+                multi={false}
+                onChange={(option: any) => {
+                  this.setState({ defaultSDIPEC: option.value })
+                }}
+                value={sdiPecOptions.filter((e) => e.value === defaultSDIPEC)}
+              />
+            </div>
+            <div className="mv4">
+              <div className="t-small c-muted-2">
+                {<FormattedMessage id="invoice-data-settings.sdi-pec.info" />}
+              </div>
+            </div>
+          </React.Fragment>
+          : null
+        }
+        <div className="mv5">
           <Divider orientation="horizontal" />
         </div>
         <div className="mb5">
@@ -141,7 +165,12 @@ export default class InvoiceDataForm extends Component {
             value={personTypeOptions.filter((e) => e.value === defaultPersonType)}
           />
         </div>
-        <div className="mv6">
+        <div className="flex mv4 items-center">
+          <div className="t-small c-muted-2">
+            {<FormattedMessage id="invoice-data-settings.person-type.info" />}
+          </div>
+        </div>
+        <div className="mv5">
           <Divider orientation="horizontal" />
         </div>
         <div className="mb3">
@@ -158,7 +187,12 @@ export default class InvoiceDataForm extends Component {
             onChange={() => { this.setState({ autocompleteNameSurname: !autocompleteNameSurname }) }}
           />
         </div>
-        <div className="flex justify-content flex-row-reverse mt4">
+        <div className="flex mt4 items-center">
+          <div className="t-small c-muted-2">
+            {<FormattedMessage id="invoice-data-settings.auto-name-surname.info" />}
+          </div>
+        </div>
+        <div className="flex justify-content flex-row-reverse">
           <Button
             variation="primary"
             onClick={() => { console.log('state', this.state) }}>
